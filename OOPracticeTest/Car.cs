2080f358dbb190be6dc3d2b129b148310ea63f9a
Xiaotian WU <XWu32@slb.com>
2022-10-27 11:29:32 +0800

    test: add first test

---
+++ OOPracticeTest/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOPracticeTest
+{
+    public class Car
+    {
+        private string name;
+        private int speed;
+
+        public Car()
+        {
+        }
+
+        public Car(string v1, int v2)
+        {
+            this.name = v1;
+            this.speed = v2;
+        }
+
+        public string SpeedUp()
+        {
+            return $"{this.name}: speed up {this.speed} km/h";
+        }
+    }
+}
--- OOPracticeTest/UnitTest1.cs
+++ OOPracticeTest/UnitTest1.cs
@@ -6,10 +6,16 @@ namespace OOPracticeTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_return_message_when_speed_up_given_car_speed()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            // given
+            Car car = new Car("Cool car", 30);
+
+            // when
+            string message = car.SpeedUp();
+
+            // then
+            Assert.Equal("Cool car: speed up 30 km/h", message);
         }
     }
 }
