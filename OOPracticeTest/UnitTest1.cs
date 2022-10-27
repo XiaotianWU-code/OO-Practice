3633eacf28484f8d22ad06bafbc8f7c9aa855f20
Xiaotian WU <XWu32@slb.com>
2022-10-27 11:42:16 +0800

    test: add second case

--- OOPracticeTest/UnitTest1.cs
+++ OOPracticeTest/UnitTest1.cs
@@ -17,5 +17,18 @@ namespace OOPracticeTest
             // then
             Assert.Equal("Cool car: speed up 30 km/h", message);
         }
+
+        [Fact]
+        public void Should_return_message_when_speed_up_given_truck_speed()
+        {
+            // given
+            Car car = new Car("Big Truck", 10);
+
+            // when
+            string message = car.SpeedUp();
+
+            // then
+            Assert.Equal("Big Truck: speed up 10 km/h", message);
+        }
     }
 }
